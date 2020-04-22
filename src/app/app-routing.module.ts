26a8e949d3dc1201e6e5d3ce33db6a33a766293d
ulrike.kirsten <ulrike.kirsten@cgi.com>
2020-04-22 15:34:21 +0200

    add item page; completed routing on dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import { ItemComponent } from './item/item.component';
 
 
 const routes: Routes = [
   {
-    path: '**', redirectTo: 'dashboard'
-  },
-  {
     path: 'dashboard',
     component: DashboardComponent,
   },
+  {
+    path: 'items',
+    component: ItemComponent,
+  },
+  {path: '**', redirectTo: 'dashboard'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
