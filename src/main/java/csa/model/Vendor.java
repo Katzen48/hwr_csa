d67ec9d19e83d0ee4c37ae28776501646dec96da
Katzen48 <Katzen48@freenet.de>
2020-04-23 15:07:18 +0200

    fix

--- src/main/java/csa/model/Vendor.java
+++ src/main/java/csa/model/Vendor.java
@@ -2,6 +2,8 @@ package csa.model;
 
 import java.beans.ConstructorProperties;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
@@ -9,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
+@JsonIgnoreProperties(value={"id"}, allowGetters=true)
 @AllArgsConstructor(onConstructor_={@ConstructorProperties({"id", "name", "address", "post_code", "city", "country"})})
 public class Vendor 
 {
