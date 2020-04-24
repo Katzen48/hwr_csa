efa0a66e6e294d4ee138e18a07480998628758cd
Katzen48 <Katzen48@freenet.de>
2020-04-24 15:56:19 +0200

    added javatime parser module

--- src/main/java/csa/rest/server/ObjectMapperFactory.java
+++ src/main/java/csa/rest/server/ObjectMapperFactory.java
@@ -1,12 +1,14 @@
 package csa.rest.server;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public final class ObjectMapperFactory
 {
 	public ObjectMapper buildObjectMapper()
 	{
 		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
 		
 		return mapper;
 	}
